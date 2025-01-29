5c5f573c2c23fa3ae94d2158dee33c7e233828a5
killbyfire <killbyfiredev@gmail.com>
2025-01-29 11:21:18 +0100

    Verander link

--- rollenPaginas/directiePagina.php
+++ rollenPaginas/directiePagina.php
@@ -17,7 +17,7 @@
     <div id="directieContainer">
         <div class="directieLinkDivs" ><h2>Ritten</h2><p>Overzicht van de ritten planning</p><a class="directieLinkButton" href="./chauffeurPagina.php">Ga naar ritten</a></div>
         <div class="directieLinkDivs" ><h2>Voorraad beheer</h2><p>Voorraad beheer overzicht</p><a class="directieLinkButton" href="./magazijnMedewerkerPagina.php">Ga naar voorraad beheer</a></div>
-        <div class="directieLinkDivs" ><h2>Kledingstukken</h2><p>Overzicht van de kledingstukken</p><a class="directieLinkButton" href="./kledingPagina.php">Ga naar kledingstukken</a></div>
+        <div class="directieLinkDivs" ><h2>Artikelen</h2><p>Overzicht van de artikelen</p><a class="directieLinkButton" href="./artikelenPagina.php">Ga naar kledingstukken</a></div>
         <div class="directieLinkDivs" ><h2>Klanten</h2><p>Overzicht van alle klanten</p><a class="directieLinkButton" href="./klantenPagina.php">Ga naar klanten</a></div>
     </div>
 </body>
