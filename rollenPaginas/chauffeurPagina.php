24903924b05968781ecdcaa7eaeaa9a8043ad83b
killbyfire <killbyfiredev@gmail.com>
2025-01-29 15:13:29 +0100

    Resolve 2 conflicts

--- rollenPaginas/chauffeurPagina.php
+++ rollenPaginas/chauffeurPagina.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../styles.css" rel="stylesheet" type="text/css">
     <title>Chaffeur</title>
 </head>
-<body>
-    
-<nav>
-    <img src="../assets/logo.png" alt="logo">
-    <a href="../process/logout.php">Uitloggen</a>
-</nav>
 
+<body>
+
+    <nav>
+        <img src="../assets/logo.png" alt="logo">
+        <div class="roleTag_loguitBtn">
+            <span>Chaffeurs</span>
+            <a href="../process/logout.php">Uitloggen</a>
+        </div>
+    </nav>
+
+    <footer>
+        <p>© centrumDuurzaam</p>
+    </footer>
 </body>
+
 </html>
