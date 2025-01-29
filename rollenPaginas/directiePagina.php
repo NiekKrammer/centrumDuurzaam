0d8b5d1e9a2f0d350f574a864dbde9d6ec54e0e6
killbyfire <killbyfiredev@gmail.com>
2025-01-29 15:20:53 +0100

    Remove conflicts

--- rollenPaginas/directiePagina.php
+++ rollenPaginas/directiePagina.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../styles.css" rel="stylesheet" type="text/css">
+    <link href="../css/directiePagina.css" rel="stylesheet" type="text/css">
     <title>Directie</title>
 </head>
-
 <body>
+    
+	<nav>
+		<img src="../assets/logo.png" alt="logo">
+        <a href="../process/logout.php">Uitloggen</a>
+	</nav>
 
-    <nav>
-        <img src="../assets/logo.png" alt="logo">
-        <div class="roleTag_loguitBtn">
-            <span>Directie</span>
-            <a href="../process/logout.php">Uitloggen</a>
-        </div>
-    </nav>
-
-    <footer>
-        <p>© centrumDuurzaam</p>
-    </footer>
+    <div id="directieContainer">
+        <div class="directieLinkDivs" ><h2>Ritten</h2><p>Overzicht van de ritten planning</p><a class="directieLinkButton" href="./chauffeurPagina.php">Ga naar ritten</a></div>
+        <div class="directieLinkDivs" ><h2>Voorraad beheer</h2><p>Voorraad beheer overzicht</p><a class="directieLinkButton" href="./magazijnMedewerkerPagina.php">Ga naar voorraad beheer</a></div>
+        <div class="directieLinkDivs" ><h2>Artikelen</h2><p>Overzicht van de artikelen</p><a class="directieLinkButton" href="./artikelenPagina.php">Ga naar kledingstukken</a></div>
+        <div class="directieLinkDivs" ><h2>Klanten</h2><p>Overzicht van alle klanten</p><a class="directieLinkButton" href="./klantenPagina.php">Ga naar klanten</a></div>
+    </div>
 </body>
-
 </html>
