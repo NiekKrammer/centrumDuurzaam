3bca39dd4d4d953d68040b2ff7b53ca5d59cd559
killbyfire <killbyfiredev@gmail.com>
2025-01-30 10:24:22 +0100

    Update linkss

--- rollenPaginas/klantenPagina.php
+++ rollenPaginas/klantenPagina.php
@@ -1,66 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./styles.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h2>Klanten</h2>
-    <a href="../customer.php">Maak nieuwe klant</a>
-<?php
-include_once '../classes/db.php';
-include_once '../models/Klant.php';
+    <?php include '../includes/nav.php'; ?>
+    
+    <a href="directiePagina.php" style="margin: 8px;">&lt; Ga terug</a>
+    <div class="klantenPagina">
 
-// Instantiate database and klant object
-$database = new Database();
-$db = $database->getConnection();
+        <h1>Klanten</h1>
+        <a class="nieuw" href="../customer.php">Maak nieuwe klant</a>
 
-$klant = new Klant($db);
+        <?php
+        include_once '../classes/db.php';
+        include_once '../models/Klant.php';
 
-// Query klanten
-$stmt = $klant->getAllKlanten();
-$num = $stmt->rowCount();
+        // Instantiate database and klant object
+        $database = new Database();
+        $db = $database->getConnection();
 
-if($num > 0) {
-    echo "<table border='1'>";
-    echo "<tr>";
-    echo "<th>ID</th>";
-    echo "<th>Naam</th>";
-    echo "<th>Adres</th>";
-    echo "<th>Plaats</th>";
-    echo "<th>Telefoon</th>";
-    echo "<th>Email</th>";
-    echo "<th>Edit</th>";
-    echo "<th>Delete</th>";
-    echo "</tr>";
+        $klant = new Klant($db);
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+        // Query klanten
+        $stmt = $klant->getAllKlanten();
+        $num = $stmt->rowCount();
 
-        $editLink = "../customer.php?action=edit&id=" . $id;
-        $deleteLink = "../customer.php?action=delete&id=" . $id;
-
-        if ($active) {
+        if ($num > 0) {
+            echo "<table border='1'>";
             echo "<tr>";
-            echo "<td>{$id}</td>";
-            echo "<td>{$naam}</td>";
-            echo "<td>{$adres}</td>";
-            echo "<td>{$plaats}</td>";
-            echo "<td>{$telefoon}</td>";
-            echo "<td>{$email}</td>";
-            echo "<th><a href='$editLink'>EDIT</a></th>";
-            echo "<th><a href='$deleteLink'>DELETE</a></th>";
+            echo "<th>ID</th>";
+            echo "<th>Naam</th>";
+            echo "<th>Adres</th>";
+            echo "<th>Plaats</th>";
+            echo "<th>Telefoon</th>";
+            echo "<th>Email</th>";
+            echo "<th>Edit</th>";
+            echo "<th>Delete</th>";
             echo "</tr>";
-        }
-    }
 
-    echo "</table>";
-} else {
-    echo "<p>Geen klanten gevonden.</p>";
-}
-?>
-   
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+
+                $editLink = "../customer.php?action=edit&id=" . $id;
+                $deleteLink = "../customer.php?action=delete&id=" . $id;
+
+                if ($active) {
+                    echo "<tr>";
+                    echo "<td>{$id}</td>";
+                    echo "<td>{$naam}</td>";
+                    echo "<td>{$adres}</td>";
+                    echo "<td>{$plaats}</td>";
+                    echo "<td>{$telefoon}</td>";
+                    echo "<td>{$email}</td>";
+                    echo "<th><a href='$editLink'>EDIT</a></th>";
+                    echo "<th><a href='$deleteLink'>DELETE</a></th>";
+                    echo "</tr>";
+                }
+            }
+
+            echo "</table>";
+        } else {
+            echo "<p>Geen klanten gevonden.</p>";
+        }
+        ?>
+    </div>
+
+    <footer>
+        <p>© centrumDuurzaam</p>
+    </footer>
+
 </body>
+
 </html>
